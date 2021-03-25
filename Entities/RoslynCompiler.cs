e4f2961de97000aaaf06f9645261d0362fa0552a
Marcello Allegri <marcello.allegri@websisco.com>
2021-03-25 17:34:10 +0100

    Aggiungere i file di progetto.

---
+++ Entities/RoslynCompiler.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_demo_roslyn_Core.Entities
+{
+    public static class RoslynCompiler
+    {
+        public static object CompileCode(Window window, string formulaCode, out bool isError)
+        {
+            isError = false;
+            try
+            {
+                Microsoft.CodeAnalysis.Scripting.Script<object> script = CSharpScript.Create(formulaCode, ScriptOptions.Default.
+                    WithImports("System", "System.Collections", "OfficeOpenXml", "System.IO").
+                    WithReferences(typeof(ExcelPackage).Assembly));
+
+                Microsoft.CodeAnalysis.Scripting.ScriptState<object> result = script.RunAsync().Result;
+                return result.ReturnValue;
+            }
+            catch (CompilationErrorException ex)
+            {
+                string errors = ex.Diagnostics.Select(d => d.GetMessage()).First().ToString();
+                isError = true;
+                MessageBox.Show(window, errors,string.Empty, MessageBoxButton.OK);
+                return null;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Telerik.Windows.Controls.SyntaxEditor.Tagging.Taggers;
+
+namespace WPF_demo_roslyn_Core
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ExcelPackage.LicenseContext = LicenseContext.Commercial;
+
+            var cSharptagger = new CSharpTagger(this.syntaxEditor);
+            this.syntaxEditor.TaggersRegistry.RegisterTagger(cSharptagger);
+        }
+
+        private void ExecuteButton_Click(object sender, RoutedEventArgs e)
+        {
+            string text = this.syntaxEditor.Document.CurrentSnapshot.GetText();
+            WPF_demo_roslyn_Core.Entities.RoslynCompiler.CompileCode(this, text, out bool isError);
+
+
+        }
+    }
+}
